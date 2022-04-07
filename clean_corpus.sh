1f481ec554fa1b655212f64e0634ee05a60a08ea
Sourya Kakarla <sk5057@columbia.edu>
2022-04-07 08:01:10 +0000

    Snapshot

--- clean_corpus.sh
+++ clean_corpus.sh
@@ -3,15 +3,22 @@ LANG=C
 LANGUAGE=en_IN.utf8
 LANG=en_IN.utf8
 
-echo "STARTING STAGE 1"
-./remove_parantheses.awk $1 > stage1_out.txt
-echo "FINISHED STAGE 1"
-echo "STARTING STAGE 2"
-./remove_urls.sh stage1_out.txt > stage2_out.txt
-echo "FINISHED STAGE 2"
-echo "STARTING STAGE 3"
-perl replace_num.pl < stage2_out.txt > stage3_out.txt
-echo "FINISHED STAGE 3"
-# echo "STARTING STAGE 4"
-# ./remove_urls.sh stage3_out.txt > stage4_out.txt
-# echo "FINISHED STAGE 4"
+
+function echo_ts
+{
+	echo $1 | ts
+}
+#alias echo=echo_ts
+
+#echo_ts "aaa"
+#exit
+
+echo_ts "STARTING STAGE 1"
+./remove_parantheses.awk $1 > stage1_out.txt 2> errors.log
+echo_ts "FINISHED STAGE 1"
+echo_ts "STARTING STAGE 2"
+./remove_urls.sh stage1_out.txt > stage2_out.txt 2> errors.log
+echo_ts "FINISHED STAGE 2"
+echo_ts "STARTING STAGE 3"
+perl replace_num.pl < stage2_out.txt > stage3_out.txt 2> errors.log
+echo_ts "FINISHED STAGE 3"
--- replace_num.pl
+++ replace_num.pl
@@ -1,10 +1,14 @@
 #!/usr/bin/perl
+
+my $i=0;
 while (my $line = <>){
     while ($line =~ m/([0-9][0-9]*)/g)
     {
         my $num_in_word = qx{python3 indic-num2words/num_to_word.py $1 'ta'};
         chomp $num_in_word;
         $line =~ s/([0-9][0-9]*)/$num_in_word/;
+        #$line =~ s/([0-9][0-9]*)/aaa/;
     }
-    print $line;
+    print $i . ": " . $line;
+    $i++;
 }
