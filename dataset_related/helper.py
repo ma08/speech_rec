e0dd35257eae5710ff27f50ff11ee19629c780a6
Sourya Kakarla <sourya4@ladduu.com>
2022-05-04 10:38:29 -0400

    Add speakerid to mozillacv tamil transcript files

--- dataset_related/helper.py
+++ dataset_related/helper.py
@@ -5,6 +5,30 @@ import random
 from pathlib import Path
 mozillacv_tamil_path = "./dataset_files/commonvoice_tamil"
 def create_formatted_files_mozillacv(folder_path):
+    clientid_set = set()
+
+    for partition in "train", "dev", "test":
+        file_name = os.path.join(folder_path, f"{partition}.tsv")
+        with open(file_name, "r") as f:
+            lines = [line.rstrip().split('\t') for line in f][1:]
+            clientid_set.update([line[0] for line in lines])
+    
+    client_count = 0
+    speaker_id_dic = {}
+
+    client_dic_lines = []
+    for clientid in  clientid_set:
+        # print("{:02d}".format(number))
+        client_count_str = "{:03d}".format(client_count)
+        speaker_id_dic[clientid] = f"mcvspeaker{client_count_str}"
+        client_dic_lines.append(f"{clientid}\t{speaker_id_dic[clientid]}")
+        client_count += 1
+
+    with open(os.path.join(folder_path,f"speakerid.tsv"), 'w') as file:
+        file.write('\n'.join(client_dic_lines))
+
+
+    
     for partition in "train", "dev", "test":
         file_name = os.path.join(folder_path, f"{partition}.tsv")
         print(partition)
@@ -16,16 +40,15 @@ def create_formatted_files_mozillacv(folder_path):
             # file_path = lines[0][1]
             # sentence = lines[0][2]
             # filename_stem = Path(file_path).stem
-            output_lines = [f"{Path(line[1]).stem}\t{line[2]}" for line in lines]
+            output_lines = [f"{speaker_id_dic[line[0]]}-{Path(line[1]).stem}\t{line[2]}" for line in lines]
 
             with open(os.path.join(folder_path,f"{partition}_transcription.txt"), 'w') as file:
                 file.write('\n'.join(output_lines))
 import string     
 # Printing Inbuilt punctuation function
-print(string.punctuation)            
+# print(string.punctuation)            
 
 # Function for removing punctuation
-#https://www.geeksforgeeks.org/python-preprocessing-of-tamil-text/
 def punctuation_remove(text_data): 
     # Appending non punctuated words
     punctuation ="".join([t for t in text_data if t not in string.punctuation])  
@@ -36,7 +59,7 @@ def remove_punctuation_mozillacv(folder_path):
         file_name = os.path.join(folder_path, f"{partition}_transcription.txt")
         with open(file_name, "r") as f:
             lines = [line.rstrip().split('\t') for line in f]
-            processed_lines = [f"{line[0]} {punctuation_remove(line[1])}" for line in lines]
+            processed_lines = [f"{line[0]}\t{punctuation_remove(line[1])}" for line in lines]
             # processed_lines = [f"{punctuation_remove(line[1])}" for line in lines]
             with open(os.path.join(folder_path,f"{partition}_text_processed.txt"), 'w') as file:
                 file.write('\n'.join(processed_lines))
@@ -52,6 +75,6 @@ def remove_punctuation_mozillacv(folder_path):
 if(len(sys.argv)>1):
     pass
 else:
-    # create_formatted_files_mozillacv(mozillacv_tamil_path)
+    #create_formatted_files_mozillacv(mozillacv_tamil_path)
     remove_punctuation_mozillacv(mozillacv_tamil_path)
 
