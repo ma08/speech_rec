f9a4645024707e8c2575a94341f552c98038446c
Sourya Kakarla <sourya4@ladduu.com>
2022-05-04 01:30:49 -0400

    Add openslr function and files for split text

--- dataset_related/split_dataset.py
+++ dataset_related/split_dataset.py
@@ -4,6 +4,34 @@ import random
 
 microsoft_train_file = "./dataset_files/microsoft_tamil/train_transcription.txt"
 
+openslr_text_corpus_file="./dataset_files/openslr_tamil/combined.tsv"
+
+def create_dev_test_train_from_full(text_file_name):
+    #Get parent directory of text_file_name
+    parent_dir = os.path.dirname(text_file_name)
+    
+
+
+    with open(text_file_name, "r") as f:
+        lines = [line.rstrip() for line in f]
+        random.shuffle(lines)
+        total_num_lines = len(lines)
+        dev_num_lines = int(total_num_lines * 0.01)
+        test_num_lines = int(total_num_lines * 0.01)
+
+        dev_lines = lines[:dev_num_lines]
+        test_lines = lines[dev_num_lines:dev_num_lines+test_num_lines]
+        new_train_lines = lines[dev_num_lines+test_num_lines:]
+
+        with open(os.path.join(parent_dir,"dev_transcription.txt"), 'w') as file:
+            file.write('\n'.join(dev_lines))
+
+        with open(os.path.join(parent_dir,"test_transcription.txt"), 'w') as file:
+            file.write('\n'.join(dev_lines))
+
+        with open(os.path.join(parent_dir,"new_train_transcription.txt"), 'w') as file:
+            file.write('\n'.join(new_train_lines))
+
 #For microsoft speech corpus
 def create_dev_partition_from_train(text_file_name):
     #Get parent directory of text_file_name
@@ -28,6 +56,6 @@ def create_dev_partition_from_train(text_file_name):
 if(len(sys.argv)>1):
     create_dev_partition_from_train(sys.argv[1])
 else:
-    create_dev_partition_from_train(microsoft_train_file)
-
+    # create_dev_partition_from_train(microsoft_train_file)
+    create_dev_test_train_from_full(openslr_text_corpus_file)
 
