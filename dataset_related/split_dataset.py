b96bf5fe04df8fee62712d6af8bd476d94042b32
Sourya Kakarla <sourya4@ladduu.com>
2022-05-04 10:15:12 -0400

    Fix sorting in openslr transcripts

--- dataset_related/split_dataset.py
+++ dataset_related/split_dataset.py
@@ -19,18 +19,23 @@ def create_dev_test_train_from_full(text_file_name):
         dev_num_lines = int(total_num_lines * 0.01)
         test_num_lines = int(total_num_lines * 0.01)
 
-        dev_lines = lines[:dev_num_lines]
-        test_lines = lines[dev_num_lines:dev_num_lines+test_num_lines]
-        new_train_lines = lines[dev_num_lines+test_num_lines:]
+        dev_lines = [line.split("\t") for line in lines[:dev_num_lines]]
+        dev_lines = sorted(dev_lines, key=lambda x: x[0])
+        test_lines = [line.split("\t") for line in lines[dev_num_lines:dev_num_lines+test_num_lines]]
+        test_lines = sorted(test_lines, key=lambda x: x[0])
+        new_train_lines = [line.split("\t") for line in lines[dev_num_lines+test_num_lines:]]
+        new_train_lines = sorted(new_train_lines, key=lambda x: x[0])
 
         with open(os.path.join(parent_dir,"dev_transcription.txt"), 'w') as file:
-            file.write('\n'.join(dev_lines))
+            file.write('\n'.join(["\t".join(line) for line in dev_lines]))
 
         with open(os.path.join(parent_dir,"test_transcription.txt"), 'w') as file:
-            file.write('\n'.join(dev_lines))
+            # file.write('\n'.join(test_lines))
+            file.write('\n'.join(["\t".join(line) for line in test_lines]))
 
-        with open(os.path.join(parent_dir,"new_train_transcription.txt"), 'w') as file:
-            file.write('\n'.join(new_train_lines))
+        with open(os.path.join(parent_dir,"train_transcription.txt"), 'w') as file:
+            # file.write('\n'.join(new_train_lines))
+            file.write('\n'.join(["\t".join(line) for line in new_train_lines]))
 
 #For microsoft speech corpus
 def create_dev_partition_from_train(text_file_name):
