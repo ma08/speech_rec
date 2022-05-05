63ccb46920b083732f2e593799e77677f3abac15
Sourya Kakarla <sk5057@columbia.edu>
2022-05-05 05:47:48 +0000

    Fix bugs in create_spk2utt.sh

--- dataset_related/create_spk2utt.sh
+++ dataset_related/create_spk2utt.sh
@@ -1,10 +1,11 @@
-db_folder="~/kaldi/egs/tamil_telugu_proj/s5_r3/db"
+db_folder=~/kaldi/egs/tamil_telugu_proj/s5_r3/db
+proj_root_folder=~/kaldi/egs/tamil_telugu_proj/s5_r3/
 # for dataset in microsoft_tamil  mozillacv_tamil  openslr_tamil; do
 for dataset in openslr_tamil; do
     for parition in dev train test; do
         echo "running on $dataset $parition"
-        dir="$db_folder/$dataset/$parition/transcription"
-        cat $dir/utt2spk | utils/utt2spk_to_spk2utt.pl > $dir/spk2utt
+        dir=$db_folder/$dataset/transcription/$parition
+        cat $dir/utt2spk | $proj_root_folder/utils/utt2spk_to_spk2utt.pl > $dir/spk2utt
     done
 done
 
