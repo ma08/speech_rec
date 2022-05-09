8b254ae7176dab0c2a5d84629c7ddf9e49eb8323
Sourya Kakarla <sk5057@columbia.edu>
2022-05-09 13:03:55 +0000

    Stage 5 of cleaning done
    -expecting this to be last stage
    -TODO: testing on the 25k line random sample if it fits the requirements

--- lang_model/preprocessing/clean_corpus.sh
+++ lang_model/preprocessing/clean_corpus.sh
@@ -16,11 +16,19 @@ function echo_ts
 echo_ts "STARTING STAGE 1"
 ./remove_parantheses.awk $1 > stage1_out.txt 2> errors.log
 echo_ts "FINISHED STAGE 1"
+
 echo_ts "STARTING STAGE 2"
 ./remove_urls.sh stage1_out.txt > stage2_out.txt 2> errors.log
 echo_ts "FINISHED STAGE 2"
+
 echo_ts "STARTING STAGE 3"
 ./replace_num.sh stage2_out.txt stage3_out.txt
 echo_ts "FINISHED STAGE 3"
 
-./remove_chars.sh stage3_out.txt stage4_out.txt
+echo_ts "STARTING STAGE 4"
+./remove_chars.sh stage3_out.txt > stage4_out.txt
+echo_ts "FINISHED STAGE 4"
+
+echo_ts "STARTING STAGE 5"
+./fix_whitespace.sh stage4_out.txt > stage5_out.txt
+echo_ts "FINISHED STAGE 5"
--- lang_model/preprocessing/remove_chars.sh
+++ lang_model/preprocessing/remove_chars.sh
@@ -8,9 +8,10 @@
 #U+00A0 : NO-BREAK SPACE [NBSP]
 #U+0020 : SPACE [SP]
 
-# perl -CS -pe 's/[^\x{0B80}-\x{0BFF}\s\.]+//g' < $1
+perl -CS -pe 's/[^\x{0B80}-\x{0BFF}\s\.]+//g' < $1
+
 # perl -CSDA -plE 's/[\s\x{00A0}\x{0020}]+/ /g' $1 | tr . '\n' < $1 | sed -E '/^\s*$/d;s/^[ \t]*//;s/[ \t]*$//;s/[ \t]+/ /g;/^[[:space:]]*$/d' 
 # perl -CSDA -plE 's/[\s\x{00A0}\x{0020}]+/ /g' $1 | tr . '\n' | sed -E 's/^[ \t]*//;s/[ \t]*$//;/^[[:space:]]*$/d' 
-perl -CSDA -plE 's/[\s\x{00A0}\x{0020}]+/ /g' $1 | tr . '\n' | sed -E 's/^[ \t]*//;s/[ \t]*$//;/^[[:space:]]*$/d' | grep -E "\s{1,}"
+#perl -CSDA -plE 's/[\s\x{00A0}\x{0020}]+/ /g' $1 | tr . '\n' | sed -E 's/^[ \t]*//;s/[ \t]*$//;/^[[:space:]]*$/d' | grep -E "\s{1,}"
 
 #https://stackoverflow.com/a/48163155/3465519
