1c4570fab3e9847081fce0720268638bce945209
Sourya Kakarla <sourya4@ladduu.com>
2022-06-12 13:18:15 -0400

    Add script for spk2utt telugu

---
+++ dataset_related/create_spk2utt_telugu.sh
@@ -0,0 +1,11 @@
+db_folder=~/kaldi/egs/tamil_telugu_proj/s5_r3/db
+proj_root_folder=~/kaldi/egs/tamil_telugu_proj/s5_r3/
+for dataset in microsoft_telugu  openslr_telugu; do
+    for parition in dev train test; do
+        echo "running on $dataset $parition"
+        dir=$db_folder/$dataset/transcription/$parition
+        cat $dir/utt2spk | $proj_root_folder/utils/utt2spk_to_spk2utt.pl > $dir/spk2utt
+    done
+done
+
+
