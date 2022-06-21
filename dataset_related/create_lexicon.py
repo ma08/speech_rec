ff1f042ae7e0cc33b0e08621db1126e9c47f913a
Sourya Kakarla <sourya4@ladduu.com>
2022-06-21 12:33:48 -0400

    Add new lexicon method using indicnlp

--- dataset_related/create_lexicon.py
+++ dataset_related/create_lexicon.py
@@ -17,7 +17,7 @@ from tamil import utf8
 similar to phonemes
 """
 def split_word_to_letters(tamil_word):
-	return utf8.get_letters(tamil_word)
+    return utf8.get_letters(tamil_word)
 
 def get_lexicon_tamil(folder_path):
     folder_path_orig  = folder_path
@@ -77,7 +77,33 @@ def get_lexicon_telugu(folder_path):
 
     print(f"Total words: {total_count}, non tamil count {non_telugu_count} set count: {len(telugu_word_set)}")
 
-            # print(lines)
+def create_new_tamil_lexicon(folder_path):
+    folder_path_orig  = folder_path
+    folder_path = os.path.expanduser(folder_path)
+    tamil_word_set = set()
+    total_count = 0
+    non_tamil_count = 0
+    for partition in "train", "dev", "test":
+        cur_folder_path = f"{folder_path}/{partition}"
+        cur_text_file = f"{cur_folder_path}/text"
+        with open(cur_text_file, "r") as f:
+            lines = [line.split()[1:] for line in f]
+            for line in lines:
+                for word in line:
+                    total_count+=1
+                    if(not utility_module.check_if_tamil_word(word)):
+                        # print(f"{word} is not a tamil word")
+                        non_tamil_count +=1
+                    else:
+                        tamil_word_set.add(word)
+    
+    with open(f"{folder_path}/new_tamil.dic", 'w') as file:
+        for telugu_word in tamil_word_set:
+            letters = utility_module.split_word_to_letters(telugu_word, 'ta')
+            file.write(f"{telugu_word}\t{' '.join(letters)}\n")
+
+
+    print(f"Total words: {total_count}, non tamil count {non_tamil_count} set count: {len(tamil_word_set)}")
 
 def check_lexicon(file_path, lang='te'):
     count=0
@@ -103,10 +129,11 @@ else:
     # kaldi_db_telugu = "~/kaldi/egs/tamil_telugu_proj/s5_r3/db/telugu_combined_transcription"
     # get_lexicon_telugu(kaldi_db_telugu)
 
-    check_lexicon("/home/sourya4/pro/columbia/spring22/fund_sp_rec/speech_rec_repo/kaldi_tamtel_db/telugu_combined_transcription/telugu.dic")
-	# print(f"print(split_word_to_letters('நட‌க்க')): {split_word_to_letters('நட‌க்க')}")
-	# print(f"print(split_word_to_letters('அதிகாரி﻿')): {split_word_to_letters('அதிகாரி﻿')}")
-	# print(split_word_to_letters("நட‌க்க"))
+    # check_lexicon("/home/sourya4/pro/columbia/spring22/fund_sp_rec/speech_rec_repo/kaldi_tamtel_db/telugu_combined_transcription/telugu.dic")
+    # print(f"print(split_word_to_letters('நட‌க்க')): {split_word_to_letters('நட‌க்க')}")
+    # print(f"print(split_word_to_letters('அதிகாரி﻿')): {split_word_to_letters('அதிகாரி﻿')}")
+    # print(split_word_to_letters("நட‌க்க"))
     #remove_punctuation_combined(kaldi_db)
 
+    create_new_tamil_lexicon("/home/sourya4/pro/columbia/spring22/fund_sp_rec/speech_rec_repo/kaldi_tamtel_db/combined_transcription")
 
